937d58728ac8e10563a2b7069b3deeb500c8a5f7
avi7167 <55663683+avi7167@users.noreply.github.com>
2022-02-07 14:19:43 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+
+sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt-get install -y --reinstall build-essential
+sudo apt-get install -y python3-pip
+sudo apt-get install -y dnsutils
+sudo apt-get install -y lua5.1 alsa-utils libpq5
+sudo apt-get install -y python3;
+sudo apt-get install -y python3-pip;
+sudo apt-get install -y ruby;
+sudo apt-get install -y screen;
+sudo apt-get install -y libcurl4-openssl-dev
+sudo apt-get install -y libssl-dev
+sudo apt-get install -y jq
+sudo apt-get install -y libpcap-dev
+sudo apt-get install -y ruby-full
+sudo apt-get install -y libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev build-essential libgmp-dev zlib1g-dev
+sudo apt-get install -y build-essential libssl-dev libffi-dev
+sudo apt-get install -y rename
+sudo apt-get autoremove -y
+sudo apt clean -y
+
+golang_install(){
+  wget https://go.dev/dl/go1.17.6.linux-amd64.tar.gz
+  sudo tar -C /usr/local -xzf go1.17.6.linux-amd64.tar.gz
+  sudo echo 'export GOROOT=/usr/local/go' >> ~/.bashrc 
+  sudo echo 'export GOPATH=$HOME/go' >> ~/.bashrc 
+  sudo echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bashrc
+  source ~/.bashrc
+  if [[ $(go version | grep -o '1.17') == 1.17 ]]; then
+  echo -e "Go is installed Successfully"
+  fi
+
+}
+
+projectdiscovery_tools(){
+    go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+    nuclei update templates
+    go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+    go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
+    go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+    go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest
+    go install -v github.com/projectdiscovery/notify/cmd/notify@latest
+    GO111MODULE=on go get -v github.com/projectdiscovery/mapcidr/cmd/mapcidr
+    
+}
+
+tomnomnom_tools(){
+    go install -v github.com/tomnomnom/waybackurls@latest
+    go install -v github.com/tomnomnom/httprobe@latest
+    go install -v github.com/tomnomnom/assetfinder@latest
+    go install -v github.com/tomnomnom/qsreplace@latest
+    go install -v github.com/tomnomnom/unfurl@latest
+    go install -v github.com/tomnomnom/gf@latest | gf -h 
+    echo 'source $GOPATH/pkg/mod/github.com/tomnomnom/gf*/gf-completion.bash' >> ~/.bashrc
+    source ~/.bashrc
+    sudo cp -r $GOPATH/pkg/mod/github.com/tomnomnom/gf*/examples ~/.gf
+}
+
+
+golang_install;
+tomnomnom_tools;
+projectdiscovery_tools;
